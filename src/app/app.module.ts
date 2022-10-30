01a8510b3d6ed84eda212335bcd733b9b8986784
Guillermo <guillef33@gmail.com>
2022-10-30 18:44:25 -0300

    inicio aiko hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HeroSection } from './components/hero/hero-section.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroSection,
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/hero/hero-section.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  templateUrl: './hero-section.component.html',
+  styleUrls: ['./hero-section.component.scss']
+})
+export class HeroSection implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
