462461cee62b2c96ca3a483d6fc3eb1a74266d3e
joroya.hms <47667368+joshuacba08@users.noreply.github.com>
2022-11-08 00:33:26 -0300

    integración con mk

--- src/app/documents/pages/get-started/get-started.component.ts
+++ src/app/documents/pages/get-started/get-started.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetStartedComponent implements OnInit {
 
+
+//   ngxMarkdownVersion = '14.0.1';
+
+//   markdown = `# Markdown __rulez__!
+// ---
+
+// ### Syntax highlight
+// '''typescript
+
+// const language = 'typescript';
+// '''
+
+// ### Lists
+// 1. Ordered list
+// 2. Another bullet point
+//    - Unordered list
+//    - Another unordered bullet
+
+// ### Blockquote
+// > Blockquote to the max`;
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/documents/pages/get-started/install.md
@@ -0,0 +1,16 @@
+# __Instalacion__
+
+---
+
+## CDN
+
+Para instalar nuestra herramienta a traves de npm, bastara con copiar
+
+```html
+
+  <script> src="algunlugar">
+
+
+  </script>
+
+```
